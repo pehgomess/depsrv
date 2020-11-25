118e6d7905dc4ec0d5ff3ad036b2efffa1fd7092
pehgomess <pehgomess@gmail.com>
2020-11-25 09:37:39 -0300

    first commit

---
+++ models/servidor.py
@@ -0,0 +1,86 @@
+import re
+from sql_alchemy import banco
+
+listso = ['rhel', 'ubuntu', 'centos']
+so = [soformat.strip().lower() for soformat in listso]
+
+class ServidorModel(banco.Model):
+    __tablename__ = 'servidores'
+
+    servidor_id = banco.Column(banco.String, primary_key=True)
+    hostname = banco.Column(banco.String(20))
+    tipo = banco.Column(banco.String(10))
+    so = banco.Column(banco.String(10))
+    macaddress = banco.Column(banco.String(20))
+    ipaddress = banco.Column(banco.String(20))
+    netmask = banco.Column(banco.String(20))
+    gateway = banco.Column(banco.String(20))
+
+    def __init__(self, servidor_id, hostname, tipo, so, macaddress, ipaddress, netmask, gateway):
+        self.servidor_id = servidor_id.lower()
+        if len(self.servidor_id) == 12:
+            len(self.servidor_id)
+        else:
+            print("O nome do servidor precisa possuir 12 caracteres o valor atual e de {}".format(len(self.__hostname)))
+            exit(1)
+        self.tipo = tipo.lower()
+        self.so = so.lower()
+        if self.so in so:
+            self.so
+        else:
+            return {'message': 'Erro para cadastrar o servidor, o S.O : {} nao esta na lista '.format(self.so)}, 404
+        return servidor.json(), 404
+        self.hostname = hostname.lower()
+        if len(self.hostname) == 12:
+            len(self.hostname)
+        else:
+            return {'message': 'O nome do servidor precisa possuir 12 caracteres o valor atual e de {}'.format(len(self.__hostname))}
+        return servidor.json(), 404
+        self.macaddress = macaddress
+        self.macaddress = re.sub('[.:-]', '', self.macaddress).lower()
+        self.macaddress = ''.join(self.macaddress.split())
+        assert len(self.macaddress) == 12
+        assert self.macaddress.isalnum()
+        self.macaddress = ":".join(["%s" % (self.macaddress[i:i+2])
+                                      for i in range(0, 12, 2)])
+        self.macaddress
+        self.ipaddress = ipaddress
+        self.netmask = netmask
+        self.gatewayinst = gatewayinst
+
+
+    def json(self):
+        return {
+            'servidor_id': self.hotel_id,
+            'hostname': self.nome,
+            'tipo': self.estrelas,
+            'so': self.diaria,
+            'macaddress': self.cidade,
+            'ipaddress': self.ipaddress,
+            'netmask': self.netmask,
+            'gateway': self.gateway
+            }
+
+    @classmethod #decorador
+    def find_servidor(cls, servidor_id):
+        servidor = cls.query.filter_by(servidor_id=servidor_id).first() #SELECT * FROM hotels WHERE hotel_id = $hotel_id LIMIT 1
+        if servidor:
+            return servidor
+        return None
+
+    def save_servidor(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_servidor(self, hostname, tipo, so, macaddress, ipaddress, netmask, gateway):
+        self.hostname = hostname
+        self.tipo = tipo
+        self.so = so
+        self.macaddress = macaddress
+        self.ipaddress = ipaddress
+        self.netmask = netmask
+        self.gateway = gateway
+
+    def delete_servidor(self):
+        banco.session.delete(self)
+        banco.session.commit()
---
+++ resources/servers.py
@@ -0,0 +1,58 @@
+from flask_restful import Resource, reqparse
+from models.servidor import ServidorModel
+
+class Servidores(Resource):
+    def get(self):
+        return {'servidores': [servidor.json() for servidor in ServidorModel.query.all()]}
+
+class Servidor(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('hostname', type=str, required=True, help="O hostname e necessario para a criacao do servidor")
+    argumentos.add_argument('tipo')
+    argumentos.add_argument('SO')
+    argumentos.add_argument('macaddress'type=str, required=True, help="O macaddress e necessario para a criacao do servidor")
+    argumentos.add_argument('ipaddress')
+    argumentos.add_argument('netmask')
+    argumentos.add_argument('gateway')
+
+    def get(self, servidor_id):
+        servidor = ServidorModel.find_servidor(servidor_id)
+        if servidor:
+            return servidor
+        return {'message': 'servidor not found.'}, 404
+
+    def post(self, servidor_id):
+        if ServidorModel.find_servidor(servidor_id):
+            return {"message": "servidor id '{}'. existe.".format(servidor_id)}, 400
+
+        dados = Servidor.argumentos.parse_args()
+        servidor = ServidorModel(servidor_id, **dados)
+        try:
+            servidor.save_servidor()
+        except:
+            return {'message': 'Erro ao salvar no banco de dados, validar a comunicacao com o banco.'}, 500
+        return servidor.json()
+
+    def put(self, servidor_id):
+        dados = Servidor.argumentos.parse_args()
+        servidor_encontrado = ServidorModel.find_servidor(servidor_id)
+        if servidor_encontrado:
+            servidor_encontrado.update_servidor(**dados)
+            servidor_encontrado.save_hotel()
+            return servidor_encontrado.json(), 200
+        servidor = ServidorModel(servidor_id, **dados)
+        try:
+            servidor.save_servidor()
+        except:
+            return {'message': 'Erro ao salvar no banco de dados, validar a comunicacao com o banco.'}, 500
+        return servidor.json(), 201
+
+    def delete(self, hotel_id):
+        servidor = ServidorModel.find_servidor(servidor_id)
+        if servidor:
+            try:
+                servidor.delete_servidor()
+            except:
+                return {'message': 'Erro ao salvar no banco de dados, validar a comunicacao com o banco.'}, 500
+            return: {'message': 'servidor deletado.'}
+        return {'message': 'servidor nao existe'}, 404
